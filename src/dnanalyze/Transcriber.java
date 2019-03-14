99ff023684918a703a898f9a7cc9261dcf9ba8c8
Daniel Kramer <drk98@nau.edu>
2019-03-13 17:30:27 -0700

    Added base functions

--- src/dnanalyze/Transcriber.java
+++ src/dnanalyze/Transcriber.java
@@ -2,5 +2,57 @@ package dnanalyze;
 
 public class Transcriber
 {
+    private String dnaString;
 
+    /**
+     * Constructor
+     * 
+     * @param dnaString
+     *            A string of the DNA that needs to be transcribed
+     */
+    public Transcriber(String dnaString)
+    {
+        this.dnaString = dnaString.toUpperCase();
+    }
+
+    /**
+     * Transcribes the DNA
+     * 
+     * @return The class' DNA string transcribed
+     */
+    public String Transcribe()
+    {
+        return Transcribe(dnaString);
+    }
+
+    /**
+     * Transcribes the DNA
+     * 
+     * @param dnaString
+     *            A string of the DNA that needs to be transcribed
+     * @return The transcribed DNA
+     */
+    public String Transcribe(String dnaString)
+    {
+        dnaString = replaceChar(dnaString, 'T', 'U');
+
+        return dnaString;
+    }
+
+    /**
+     * Replaces all occurrences of one char with another in the string
+     * 
+     * @param workingString
+     *            The string to replace the char in
+     * @param toReplace
+     *            The char in the string to replace
+     * @param toReplaceWith
+     *            The char to replace it with
+     * @return
+     */
+    private String replaceChar(String workingString, char toReplace,
+            char toReplaceWith)
+    {
+        return null;
+    }
 }
