9dbb99a9129adc7183612256e3909245a1f8ab3d
Daniel Kramer <drk98@nau.edu>
2019-04-01 17:18:22 -0700

    Added base things for the user

--- src/dnanalyze/User.java
+++ src/dnanalyze/User.java
@@ -2,5 +2,15 @@ package dnanalyze;
 
 public abstract class User
 {
+    String name;
 
+    int UID;
+
+    public User()
+    {
+        this.name = "";
+        UID = -1;
+    }
+
+    protected abstract void getInfoFromDB();
 }
