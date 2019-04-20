e443ae5ffb40b8650cff833b38e54ff32a103e3c
Daniel Kramer <drk98@nau.edu>
2019-04-19 19:36:34 -0700

    Revert "FlashCard Update"
    This reverts commit cb44779878e1a87856a9e0e03a6a39795f6845fb.

--- src/dnanalyze/Flashcard.java
+++ src/dnanalyze/Flashcard.java
@@ -1,49 +1,6 @@
 package dnanalyze;
 
-import java.util.LinkedList;
-
-
 public class Flashcard extends QuestionAnswer
 {
-  String savePrompt= "Are you sure you would like to save?";
-  String addTermPrompt = "Enter Term: ";
-  String addDefPrompt ="Enter Definition: ";
-
-  int numOfCards= 15;
-  String [] termsArray = new String(numOfCards);
-  String [] definitionsArray = new String(numOfCards);
-
-  int tailOfTerms= numOfCards;
-  int tailofDefs = numOfCards;
-  int numOfCurrentCard= 0;
-
-
-
-  public void addCard()
-  {
-    System.out.println(addTermPrompt);
-    termsArray[tailOfTerms] = input.next();
-    System.out.println(addDefPrompt);
-    definitionsArray[tailOfDefs] = input.next();
-  }
-
-  public void deleteCard(int numOfCurrentCard)
-  {
-    int index;
-
-    for ( index = numOfCurrentCard; index < termsArray.length(); index++)
-    {
-      termsArray[index] = termsArray[index+1];
-    }
-
-    for ( index = numOfCurrentCard; index < definitionsArray.length(); index++)
-    {
-      definitionsArray [index] = definitionsArray[index+1];
-    }
-
-  }
-
-
-
 
 }
